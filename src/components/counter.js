775aa4b789d130706f3b62c55398e8013efbdb14
i1-kmv <i1-kmv@mail.ru>
2020-08-25 10:43:24 +0200

    ready funny counter

--- src/components/counter.js
+++ src/components/counter.js
@@ -22,15 +22,9 @@ const mapStateToProps = (state) => {
 }
 
 const mapDispatchToProps = (dispatch) => {
-    const {inc, dec, rnd } = bindActionCreators(actions, dispatch)
-    return {
-        inc,
-        dec,
-        rnd: () => {
-            const randomValue = Math.floor(Math.random()*10)
-            rnd(randomValue)
-        },
-    }
+
+    return bindActionCreators(actions, dispatch)
+
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(Counter)
